5045b983914ff19de5efb6dc9fc51a18dbdf885f
Luca Cermenati <luca.cermenati@expereo.com>
2021-10-02 14:41:46 +0200

    initial commit

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class QuestionController extends Controller
+{
+    public function listAllQuestions()
+    {
+        return;
+    }
+
+    public function createQuestion()
+    {
+        return;
+    }
+
+    public function updateQuestion()
+    {
+        return;
+    }
+}
